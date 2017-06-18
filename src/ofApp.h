2bfb94e251dd02cec03e6606c94bc78e85983928
Patrick Campbell <patrick.campbell@multiscalehn.com>
2017-06-18 16:03:25 -0700

    initializing the project

---
+++ src/ofApp.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+// listen on port 12346
+#define PORT 12346
+#define NUM_MSG_STRINGS 20
+
+class ofApp : public ofBaseApp {
+	public:
+
+		void setup();
+		void update();
+		void draw();
+		// void printProperties(ofImage receivedImage);
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofTrueTypeFont font;
+		ofxOscReceiver receiver;
+
+		int current_msg_string;
+		string msg_strings[NUM_MSG_STRINGS];
+		float timers[NUM_MSG_STRINGS];
+
+		int mouseX, mouseY;
+		float shapeRad, numPoints;
+		string mouseButtonState;
+    
+        ofImage receivedImage;
+};
